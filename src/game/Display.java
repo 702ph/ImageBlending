cd1f25f40df04ad843905955f10e2c165f90434f
Shuya Fuchigami <shuyafuchigami@shmcbkpro17.local>
2018-11-02 11:06:12 +0100

    various changes

--- src/game/Display.java
+++ src/game/Display.java
@@ -23,16 +23,20 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 
 	// Parameter
 	int imageSet = 4;  // 0 - 7 //default:3(ABC..), 4:pics(A~...) //which pics should be used.
-	int numPics = 4;          //default:4 ..number of pics.
+	int numPics = 3;          //default:4 ..number of pics.
 	int numOnes = 2;   // 0 < numOnes < numPics //default:2. overlay level. 2=overlay with two pics.
 
 	// true: generiere Basisbilder, die die gelesenen Eingangsbilder erzeugen  //読み込みと同時に2段目に合成画像を生成する。それらをBasis Bilderとして用いる。重ね合わせることで本来の入力画像と同様の画像を得られる。入力画像は1段目に表示される。
 	// false: verwende die Bilder als Basisbilder und erzeuge Kombinatioen //読み込んだ画像を2段めのBasic Bilderとしてそのまま使用し、1段目は生成する。
-	boolean doGenerate = true
-			;
+	boolean doGenerate = true;
 
 	// 0.51 .. 2.01 // wie stark darf ein Bild in der Linearkombination verwendet werden //default==2
-	double maxWeight = 2; //how does it work? looks like no differences
+	// <1.0 neither work.
+
+	// assumption: if 1 is used for creation, the inversed matrix has values which are bigger than 1
+	// the number must not exceed maxWeight;
+	// * maxWeight is not used at the time of array creation.
+	double maxWeight = 1; //how does it work? Pictures look like no difference
 
 	////////////////////////////////////////////////////////////////////////////////
 	String[] imageNames = {
@@ -49,8 +53,8 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 
 	};
 
-    int width, height;
-	double[][] mCorrect;    // richtige Kombination der Basisbilder (0/1) 
+	int width, height;
+	double[][] mRandom;    // richtige Kombination der Basisbilder (0/1) 
 	double[][] mInv; // Kombination der Eingangsbilder zur Erzeugung der Basisbilder
 	double [] wUser = new double[numPics];; // Kombination des Nutzers (selected pictures by mouse click)
 	//double [] wUser = new double[4];
@@ -71,8 +75,14 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 
 	//constructor
 	public Display() {
+
 		super();
 
+		//debug
+		//System.out.println( Math.abs(1) > maxWeight );
+		//generateRandomMatrix();
+
+		
 		targetImages = new BufferedImage[numPics];
 
 		this.setFocusable(true);
@@ -157,43 +167,35 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 
 	//for generate basis from input images
 	private void calculateBasisImages() {
-		
-		findCombinations();   // finde eine Konfiguration m mit Zeilensummen von minv > 0 
-				
-		//これでもうごく！！！！
-		
-		//generateRandomM();
-		//mInv = Inverse.invert(mOrig);
 
-		for(int i=0; i<mCorrect.length;i++) {
-			System.out.println(Arrays.toString(mCorrect[i]));
-		}
-		for(int i=0; i<mInv.length;i++) {
-			System.out.println(Arrays.toString(mInv[i]));
-		} 
-		
-		
-		//これはうごかん。
-		//mInv = mCorrect;
-		
-		
+		findCombinations();   // finde eine Konfiguration m mit Zeilensummen von minv > 0 
+
+		// or let it work without boundaries check.
+		//generateRandomM();
+		//mInv = Inverse.invert(mRandom);
+
+		//debug
+		matrixToString(mRandom);
+		matrixToString(mInv);
+
+
 		int[][] pixelsBasis = new int[numPics][];
 		basisPixels3 = new double[numPics][][];  // basisPixels3[Bildnummer][Position][Kanal]
 
 		basisImages = new BufferedImage[numPics];    // Basisbilder zum Anzeigen
 		for (int i = 0; i < numPics; i++) {
-			
+
 			//TOO: basisPixels3 これどこで使われてる？？？ →これでadditive mischungの作業をしている。
 			basisPixels3[i] = blendPixelsTo3DDoubleImage(targetPixels, mInv[i]);
 			// or, simply separate channel -> NO. at basis generation targetPixels have to be mixed.
 			//basisPixels3[i] = pixelToRGB(targetPixels,i);
-			
+
 			//this blendPixelsToPixels accepts only int[][]　→これは表示用。このあと加工はされない。
 			pixelsBasis[i]  = blendPixelsToPixels(targetPixels, mInv[i]);
-			
+
 			//thus this doesn't work. it accepts double[][]
 			//pixelsBasis[i] = blend3DDoubleToPixels(targetPixels, mInv[i]);
-			
+
 			basisImages[i] = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB); 	//initialize BufferedImage for eachbasisImages[i]
 			basisImages[i].setRGB(0, 0, width, height, pixelsBasis[i], 0, width); //set RGB values from pixelsBasis[i] to the BufferedImages
 		}
@@ -230,8 +232,8 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 
 		targetPixels = new int[numPics][width*height];
 		for (int i = 0; i < targetPixels.length; i++) {
-			targetPixels[i] = blend3DDoubleToPixels(basisPixels3, mCorrect[i]);
-			
+			targetPixels[i] = blend3DDoubleToPixels(basisPixels3, mRandom[i]);
+
 			targetImages[i] =  new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 			targetImages[i].setRGB(0, 0, width, height, targetPixels[i], 0, width);
 			//targetImages[i].setRGB(0, 0, width, height, basisPixels3[i], 0, width);
@@ -260,26 +262,45 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 			generateRandomM();
 
 			success = true;
-			mInv = Inverse.invert(mCorrect);
-			
+			mInv = Inverse.invert(mRandom);
+
+			//debug
+			System.out.println("maxWeight:" + maxWeight);
+
 			for (int i = 0; i < mInv.length; i++) {
 				for (int j = 0; j < mInv[i].length; j++) {
 					double val = mInv[i][j];
-					//System.out.println(val + ", ");
-					
+					//System.out.println(String.format("%.10f", val));
+
 					if (Double.isInfinite(val) || Double.isNaN(val)) {
 						success = false; // wenn Rang zu klein ist
-					} else {if (Math.abs(val) > maxWeight) // kein Bild stärker als mit maxContribution gewichten
-						success = false;
+						//debug
+						//System.out.println("size: " + i + "," + j +": " + String.format("%.5f", val));
+
+						//TODO:　separate this for-loop so that we can know which condition incur the abortion.
+					} else { 
+						if (Math.abs(val) > maxWeight) {// kein Bild stärker als mit maxContribution gewichten
+							success = false;
+
+							//debug
+							//System.out.println("maxW: " + i + "," + j +": " + String.format("%.5f", val));
+						}
 					}
-					
+
 				}
 			}
-		} while (!success && tries++ < 10000); //just repeat 10000(default) times if it's failed
+		} while (!success && tries++ < 2); //just repeat 10000(default) times if it's failed
 
 		if (!success) {
 			System.out.println("Impossible Settings, aborting");
+			matrixToString(mInv);
 			System.exit(-1);
+		}		
+	}
+
+	private void matrixToString(double[][] m) {
+		for(int i=0; i<m.length; i++) {
+			System.out.println(Arrays.toString(m[i]));
 		}
 	}
 
@@ -287,8 +308,11 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 		boolean success;		
 		do {
 			// numOnes mal eine 1 in jede Zeile von m setzen	
-			mCorrect = new double[numPics][numPics];
-			for (int i = 0; i < mCorrect.length; i++) {
+			mRandom = new double[numPics][numPics];
+
+			double element = 1.0; // the lager the value here, the smaller the value your get in inverse matrix.
+
+			for (int i = 0; i < mRandom.length; i++) {
 
 				int extra = 0; //rand.nextInt(2);       // hier werden u.U. noch extra Einzen gesetzt 
 				//System.out.println(extra);
@@ -298,17 +322,17 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 					do {
 						index = rand.nextInt(numPics);
 					}
-					while (mCorrect[i][index] == 1);
-					mCorrect[i][index] = 1;
+					while (mRandom[i][index] == element); //TODO: ??
+					mRandom[i][index] = element;
 				}
 			}		
 			success = true;
-			
-			for (int i = 0; i < numPics; i++) {
-				for (int j = i+1; j <numPics; j++)  {
+
+			for (int i = 0; i < numPics; i++) { //first line
+				for (int j = i+1; j <numPics; j++)  { // next line
 					boolean same = true; // identische Kombinationen/Zeilen vermeiden
 					for (int k = 0; k <numPics; k++) 
-						if (mCorrect[i][k] != mCorrect[j][k])
+						if (mRandom[i][k] != mRandom[j][k])
 							same = false;
 					if (same) {
 						success = false;
@@ -324,11 +348,37 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 		//				m[i][j] /= numOnes;
 	}
 
+
+	//public for JUnit test
+	public double[][] generateRandomMatrix(){
+
+		boolean success = false;
+		double[][] matrix = new double[numPics][numPics];
+		
+		while(!success) {
+			for (int i=0; i< numPics; i++) {
+				for(int j=0; j<numPics; j++) {
+					int index = rand.nextInt(numPics);
+					matrix[i][index] = 1;  
+				}
+			}
+			success=true;
+		}
+		
+		//TODO: check if there is identity line
+
+		matrixToString(matrix);
+				
+		return new double [numPics][numPics];
+	}
+
+
+
 	private void printResult() {
 		System.out.println("Lösung:");
-		for (int i = 0; i < mCorrect.length; i++) {
-			for (int j = 0; j < mCorrect[i].length; j++) {
-				System.out.printf("%6.2f", mCorrect[i][j]);
+		for (int i = 0; i < mRandom.length; i++) {
+			for (int j = 0; j < mRandom[i].length; j++) {
+				System.out.printf("%6.2f", mRandom[i][j]);
 			}
 			System.out.println();
 		}
@@ -370,10 +420,6 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 				b += w[j]*bj;
 			}
 
-			//			r = (r - 128) / numOnes + 128;
-			//			g = (g - 128) / numOnes + 128;
-			//			b = (b - 128) / numOnes + 128;
-
 			r = Math.min(Math.max(0, fi(r)), 255);
 			g = Math.min(Math.max(0, fi(g)), 255);
 			b = Math.min(Math.max(0, fi(b)), 255);
@@ -405,51 +451,18 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 				double g = f((colorPix >>  8) & 0xFF);
 				double b = f((colorPix      ) & 0xFF);	
 
-				//1. loop: [1.0, 0.0, 0.0, 0.0]  -> 1; 0; 0; 0
-				//2. loop: [0.0, 1.0, 0.0, 0.0]  usw
+				//1. call: [1.0, 0.0, 0.0, 0.0]
+				//2. call: [0.0, 1.0, 0.0, 0.0]  usw
 				rNew += mInvParam[picNum] * r; 
 				gNew += mInvParam[picNum] * g;
 				bNew += mInvParam[picNum] * b;
-				
-				//rNew = r;
-				//gNew = g;
-				//bNew = b;
-			}
 
+			}
+			
+			//3d double image
 			pixelsOut[position][0] = fi(rNew);
 			pixelsOut[position][1] = fi(gNew);
 			pixelsOut[position][2] = fi(bNew);
-		}
-		return pixelsOut;
-	}
-
-	private double[][] pixelToRGB(int[][] targetPixelsIn, int picNo) {
-
-		System.out.println("colorToRGB():");
-		System.out.println(picNo);
-		//System.out.println(Arrays.toString(mInvParam));
-
-		//targetPixelsIn[][] == targetPixels[numPics][width*height]
-		//pixelsOut has [width*height][color channels(RGB==3)]
-		//double[][] pixelsOut = new double[targetPixelsIn[0].length][3];
-		double[][] pixelsOut = new double[width*height][3];
-
-		int numberOfPixels = pixelsOut.length;
-		for (int position = 0; position < numberOfPixels; position++) {
-			
-			int colorPix = targetPixelsIn[picNo][position];
-			double r = (colorPix >> 16) &0xFF;
-			double g = (colorPix >> 8)  &0xFF;
-			double b = (colorPix)       &0xFF;
-			
-			pixelsOut[position][0] = r;
-			pixelsOut[position][1] = g;
-			pixelsOut[position][2] = b;
-			
-			//pixelsOut[position][0] = (colorPix >> 16);
-			//pixelsOut[position][1] = (colorPix >>  8);
-			//pixelsOut[position][2] = (colorPix      );
-			
 		}
 		return pixelsOut;
 	}
@@ -463,100 +476,27 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 		return  (val + zeroLevel);
 	}
 
-
-	//not used
-	/*
-	private int[] blend3DDoubleToPixelsOrig(double[][][] pixelsIn, double[] w) {
-		int[] pixels = new int[pixelsIn[0].length];
-
-		for (int i = 0; i < pixels.length; i++) {
-			double r = 0, g = 0, b = 0;
-
-			for (int j = 0; j < pixelsIn.length; j++) {
-				double rj = f( pixelsIn[j][i][0]);
-				double gj = f( pixelsIn[j][i][1]);
-				double bj = f( pixelsIn[j][i][2]);	
-
-				r += w[j]*rj; // * numOnes;
-				g += w[j]*gj; // * numOnes;
-				b += w[j]*bj; // * numOnes;
-			}
-
-			r = Math.min(Math.max(0, fi(r) ), 255);
-			g = Math.min(Math.max(0, fi(g) ), 255);
-			b = Math.min(Math.max(0, fi(b) ), 255);
-			pixels[i] = 0xFF000000 | ((int)r <<16) | ((int)g << 8) | (int)b;
-		}
-		return pixels;
+	double applyZeroLevelMinus(double val) { //why does it have to be public??
+		return  val - zeroLevel;
 	}
-	 */
+	double applyZeroLevelPlus(double val) {
+		return  (val + zeroLevel);
+	}
+	
+	
 
 
 	private int[] blend3DDoubleToPixels(double[][][] pixelsIn, double[] w) {
-		
+
 		//w == combination of pictures, by user and or program calculated
-		
+
 		System.out.println("blend3DDoubleToPixels():");
 		System.out.println(Arrays.toString(w));
-		
+
 		//int[] pixels = new int[pixelsIn[0].length];
 		//↑ same ↓
 		int[] pixels = new int[width*height];
 
-		double rMin = 255, rMax = 0;
-		double gMin = 255, gMax = 0;
-		double bMin = 255, bMax = 0;
-
-		for (int i = 0; i < pixels.length; i++) { // i Bildposition
-			double r = 0, g = 0, b = 0;
-
-			for (int j = 0; j < pixelsIn.length; j++) {  // j = Bildnummer
-				double rj = f( pixelsIn[j][i][0]);
-				double gj = f( pixelsIn[j][i][1]);
-				double bj = f( pixelsIn[j][i][2]);	
-
-				r += w[j]*rj; 
-				g += w[j]*gj;
-				b += w[j]*bj;
-				//r += rj; 
-				//g += gj;
-				//b += bj;
-			}
-			
-			r = fi(r);
-			g = fi(g);
-			b = fi(b);
-
-			//what is this? for statistic?
-			if (r > rMax) rMax = r;
-			if (r < rMin) rMin = r;
-			if (g > gMax) gMax = g;
-			if (g < gMin) gMin = g;
-			if (b > bMax) bMax = b;
-			if (b < bMin) bMin = b;
-
-			
-			//r = Math.min(Math.max(0, r ), 255);
-			//g = Math.min(Math.max(0, g ), 255);
-			//b = Math.min(Math.max(0, b ), 255);
-
-			//pixels[i] = 0xFF000000 | ((int)r <<16) | ((int)g << 8) | (int)b;
-			
-		}
-
-
-
-		//double max = Math.max(rMax, Math.max(gMax,  bMax));
-		//double min = Math.min(rMin, Math.min(gMin,  bMin));
-
-		//System.out.println("rMin, rMax, gMin, gMax, bMin, bMax");
-		//System.out.println(rMin + "," + rMax + ", " + gMin + "," + gMax + ", " + bMin + "," + bMax );
-
-		//System.out.println("min,max");
-		//System.out.println(min + "," + max);
-
-
-		
 		for (int i = 0; i < pixels.length; i++) {
 			double r = 0, g = 0, b = 0;
 
@@ -572,10 +512,6 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 			r = fi(r);
 			g = fi(g);
 			b = fi(b);
-
-			//r = (r-min)*255/(max-min);
-			//g = (g-min)*255/(max-min);
-			//b = (b-min)*255/(max-min);
 
 			r = Math.min(Math.max(0, r ), 255);
 			g = Math.min(Math.max(0, g ), 255);
@@ -583,15 +519,11 @@ public class Display extends JPanel implements MouseListener, KeyListener {
 
 			pixels[i] = 0xFF000000 | ((int)r <<16) | ((int)g << 8) | (int)b;
 		}
-		 
 
 		return pixels;
 	}
 
-	private boolean checkIdentity(int[] a, int[] b) {
-		return a.equals(b);
-	}
-
+	
 
 	@Override
 	public void paintComponent(Graphics g) {
